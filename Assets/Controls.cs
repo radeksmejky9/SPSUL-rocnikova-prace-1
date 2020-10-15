82eeb5a769d4a6bc6b6e2d1db4e0b7233c487c8a
radeksmejky9 <radeksmejky9@gmail.com>
2020-10-15 11:59:29 +0200

    working movement, broken camera control

---
+++ Assets/Controls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)))
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                InputManager.Instance.Right = true;
+            }
+            else
+            {
+                InputManager.Instance.Right = false;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                InputManager.Instance.Left = true;
+            }
+            else
+            {
+                InputManager.Instance.Left = false;
+            }
+        }
+    }
+}
---
+++ Assets/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public bool Right;
+    public bool Left;
+}
