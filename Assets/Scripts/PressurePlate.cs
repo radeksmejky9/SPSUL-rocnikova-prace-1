5f65eacc40d8dcba6a7e86580a95a03f5c57ae08
radeksmejky9 <radeksmejky9@gmail.com>
2021-05-20 22:15:01 +0200

    Pre may

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+
+    public List<GameObject> list;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (var sphere in list)
+        {
+            sphere.AddComponent<Rigidbody>();
+            sphere.GetComponent<Rigidbody>().mass = 0.01f;
+        }
+    }
+}
---
+++ Assets/Scripts/rock.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class rock : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == GameObject.FindGameObjectWithTag("MainCharacter").GetComponent<BoxCollider>())
+        {
+            other.GetComponent<DamageDetector>().TakeDamage(3);
+        }
+    }
+}
